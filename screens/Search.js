dd1552d652e5b5527d85621a2a6e922b1c346ea5
Quoc Dung <20521208@gm.uit.edu.vn>
2023-11-02 20:51:55 +0700

    qdung lan 2

--- screens/Search.js
+++ screens/Search.js
@@ -1,15 +1,155 @@
 import React from 'react'
-import { Text, View, Button } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, TextInput,Image, FlatList,SectionList } from 'react-native';
 
+const postdata = [
+    {
+        id:1,
+        menuImage:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQueae2dB4D2UU5FxZmTTyGBr6MTKqmTfGD5g&usqp=CAU',
+        name: 'tom hum'
+    },
+    {
+        id:2,
+        menuImage:'https://chuphinhmonan.com/wp-content/uploads/2017/03/dakgalbi-1-2.jpg',
+        name: 'Bào ngư'
+    },
+    {
+        id:3,
+        menuImage:'https://assets.unileversolutions.com/v1/1187507.jpg',
+        name: 'Vây cá'
+    },
+    {
+        id:4,
+        menuImage:'https://cdn.tgdd.vn/Files/2019/03/07/1153472/anh-sang_600x800.jpg',
+        name: 'Cá ngừ đại dương'
+    },
+    {
+        id:5,
+        menuImage:'https://cdn.tgdd.vn/Files/2019/03/07/1153472/anh-sang_600x800.jpg',
+        name: 'Vây cá voi'
+    },
+    {
+        id:6,
+        menuImage:'https://cdn.tgdd.vn/Files/2019/03/07/1153472/anh-sang_600x800.jpg',
+    },
+    {
+        id:7,
+        menuImage:'https://cdn.tgdd.vn/Files/2019/03/07/1153472/anh-sang_600x800.jpg',
+    },
+    {
+        id:8,
+        menuImage:'https://cdn.tgdd.vn/Files/2019/03/07/1153472/anh-sang_600x800.jpg',
+    },
+    {
+        id:9,
+        menuImage:'https://cdn.tgdd.vn/Files/2019/03/07/1153472/anh-sang_600x800.jpg',
+    },
+    {
+        id:10,
+        menuImage:'https://cdn.tgdd.vn/Files/2019/03/07/1153472/anh-sang_600x800.jpg',
+    },
+    {
+        id:11,
+        menuImage:'https://cdn.tgdd.vn/Files/2019/03/07/1153472/anh-sang_600x800.jpg',
+    },
+    {
+        id:12,
+        menuImage:'https://cdn.tgdd.vn/Files/2019/03/07/1153472/anh-sang_600x800.jpg',
+    },
+]
+const renderImage =({item})=>(
+    <View style={styles.ImageContainer}>
+        <Image source={{ uri: item.menuImage }} style={styles.Image} />
+        <View style={styles.brText}>
+           <Text style={styles.nameImage} >{item.name} </Text>
+        </View>
+    </View>
 
+)
 const Search=()=>{
 return (
-<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-<Text> Màn Hình Chính!</Text>
-<Button
-title="Go to Details"
-/>
+<View style={styles.container}>
+    <View style={styles.search}>
+        <TextInput style={styles.searchInput}
+        placeholder='Gõ vào nguyên liệu hoặc món cần tìm'
+        />
+    </View>
+    <View style={styles.menu}>
+        <Text style={styles.textMenu}>Món tìm kiếm phổ biển hôm nay</Text>
+        <FlatList 
+        style={styles.flatList}
+        data={postdata}
+        renderItem={renderImage}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        key={'flatlist_key'}
+        />
+    </View>
 </View>
 )
 }
+const styles = StyleSheet.create({
+    container:{
+        marginTop:50,
+        backgroundColor:'#DDDDDD',
+    },
+    search:{
+        backgroundColor:'#FFFFFF',
+        height:80,
+    },
+    searchInput:{
+        marginTop:15,
+        marginLeft:30,
+        marginRight:30,
+        backgroundColor:'#DDDDDD',
+        borderRadius:15,
+        paddingLeft:15,
+        paddingRight:20,
+        height:50,
+    },
+    menu:{
+        marginTop:5,
+        backgroundColor:'#FFFFFF',
+        paddingBottom:10,
+    },
+    textMenu:{
+        fontSize:20,
+        marginTop:15,
+        marginBottom:15,
+        marginLeft:15,
+    },
+    ImageContainer:{
+        flex:1,
+        // position:'relative',
+    },
+    Image:{
+        height:110,
+        width:175,
+        borderRadius:8,
+        marginBottom:10,
+        marginRight:10,
+    },
+    menuImage:{
+        marginLeft:15,
+    },
+    flatList:{
+        height:475,
+        marginLeft:15,
+    },
+    brText:{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        backgroundColor: '#BBBBBB',
+        elevation:1,
+        borderRadius:8,
+        width:175,
+    },
+    nameImage:{
+        fontSize: 20,
+        paddingLeft:5,
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+    },
+})
+
 export default Search
--- screens/authStack.js
+++ screens/authStack.js
@@ -1,5 +1,7 @@
+// npm install @react-navigation/material-bottom-tabs react-native-paper react-native-vector-icons
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome'
 import Home from './Home';
 import Search from './Search';
 import Storages from './Storages';
@@ -7,13 +9,48 @@ import Profile from './Profile';
 
 const Tab = createBottomTabNavigator();
 
-const  Main=()=> {
+const Main = () => {
+  const Tab = createBottomTabNavigator();
   return (
-    <Tab.Navigator>
-    <Tab.Screen name="Home" component={Home} options={{header: () => null}}/>
-    <Tab.Screen name="Search" component={Search} options={{header: () => null}}/>
-    <Tab.Screen name="Storages" component={Storages} options={{header: () => null}}/>
-    <Tab.Screen name="Profile" component={Profile} options={{header: () => null}}/>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, size, color }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = 'home'
+            size = focused ? 28 : 28
+            color = focused ? '#4190f4' : '#808080'
+          } else if (route.name === 'Search') {
+            iconName = 'search'
+            size = focused ? 28 : 28
+            color = focused ? '#4190f4' : '#808080'
+          } else if (route.name === 'Storages') {
+            iconName = 'plus-circle'
+            size = focused ? 28 : 28
+            color = focused ? '#4190f4' : '#808080'
+          } else if (route.name === 'Profile') {
+            iconName = 'user'
+            size = focused ? 28 : 28
+            color = focused ? '#4190f4' : '#808080'
+          }
+          return (
+            
+            <Icon
+              name={iconName}
+              color={color}
+              size={size}
+            />
+          )
+        },
+        tabBarLabelStyle: { fontSize: 12},
+        // tabBarLabelStyle: { fontSize: 15,opacity: 0 },
+      })
+      }
+    >
+      <Tab.Screen name="Home" component={Home} options={{ header: () => null }} />
+      <Tab.Screen name="Search" component={Search} options={{ header: () => null }} />
+      <Tab.Screen name="Storages" component={Storages} options={{ header: () => null }} />
+      <Tab.Screen name="Profile" component={Profile} options={{ header: () => null }} />
     </Tab.Navigator>
   );
 }
